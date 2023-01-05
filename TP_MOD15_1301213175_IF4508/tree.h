63d13b8bc7dab30c61532a964de6eac9cc39adeb
ArrizalAryasatya <122008277+ArrizalAryasatya@users.noreply.github.com>
2023-01-05 16:48:27 +0700

    Add files via upload

---
+++ TP_MOD15_1301213175_IF4508/tree.h
@@ -0,0 +1,31 @@
+#ifndef TREE_H_INCLUDED
+#define TREE_H_INCLUDED
+
+#include <iostream>
+using namespace std;
+
+#define left(P) (P)->left
+#define right(P) (P)->right
+#define info(P) (P)->info
+#define nil NULL
+
+typedef struct ElmTree *adrNode;
+
+typedef int infotype;
+
+struct ElmTree{
+    infotype info ;
+    adrNode left, right;
+};
+
+adrNode newNode_1301213175(infotype x);
+adrNode findNode_1301213175(adrNode root, infotype x);
+void insertNode_1301213175(adrNode &root, adrNode p);
+void printPreOrder_1301213175(adrNode root);
+void printDescendant_1301213175(adrNode root, infotype x);
+int sumNode_1301213175(adrNode root);
+int countLeaves_1301213175(adrNode root);
+int heightTree_1301213175(adrNode root);
+
+#endif // TREE_H_INCLUDED
+
